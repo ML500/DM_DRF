7d5c09aee4c1640a73e5540138ed1aa2f59a9476
ML500 <emilrakaev@gmail.com>
2021-09-14 01:05:19 +0600

    init docker files

--- django_movie/settings.py
+++ django_movie/settings.py
@@ -86,13 +86,24 @@ WSGI_APPLICATION = 'django_movie.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'movie',
+#         'USER': 'postgres',
+#         'PASSWORD': '117117',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'movie',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': '117117',
-        'HOST': 'localhost',
+        'PASSWORD': '123456',
+        'HOST': 'dm_db',
         'PORT': '5432',
     }
 }
@@ -143,7 +154,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:8080",
-    # "http://127.0.0.1:8000",
+    "http://localhost:8081",
     "http://0.0.0.0:8000"
 ]
 
